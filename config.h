1ed026bf059c407b8863a6d6207bf2f623139b9b
stanwon <stan_wong@foxmail.com>
2023-07-07 20:49:56 +0800

    test lazygit

--- config.h
+++ config.h
@@ -95,7 +95,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.85, alphaUnfocused = 0.5;
+float alpha = 0.8, alphaUnfocused = 0.5;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
